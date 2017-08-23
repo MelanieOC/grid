1f38a20783fb47306b34a009a8065466fb0ac572
MelanieOC <melanie.ocharan@gmail.com>
2017-08-23 07:29:52 -0500

    mas

--- js/app.js
+++ js/app.js
@@ -1,4 +1,19 @@
 /*
  * Archivo principal de JS
  */
- console.log('hola');
+ var pictures= document.getElementsByClassName('transparente');
+
+ for (var i = 0; i < pictures.length; i++) {
+   pictures[i].onclick=function() {
+     crearModel(this);
+   }
+ }
+
+function crearModel(e) {
+  var div=document.createElement('div');
+  div.classList.add('grande');
+  var img=document.createElement('img');
+  img.src=e.nextSibling.nextSibling.src;
+  div.appendChild(img);
+  e.parentNode.appendChild(div);
+}
